a157064bd34656ee0a755b726e8d101e07582c3a
JPaigna <126860913+JPaigna@users.noreply.github.com>
2025-04-15 13:45:49 +0800

    PDP LABAN

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -8,7 +8,7 @@ const API_UPDATE_URL = 'https://backend-gg62.onrender.com/api/todos/';
 const API_DELETE_URL = 'https://backend-gg62.onrender.com/api/todos/';
 
 // Replace with your superadmin token here or set it in localStorage manually
-const TOKEN = localStorage.getItem("token") || "your-superadmin-token-here";
+const TOKEN = localStorage.getItem("token") || "4923908fee66bc05348a8b9aa46ee665bcdf0deb";
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
